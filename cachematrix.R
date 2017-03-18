5adedbb679acc8e188e94fab40f3fcb568c23260
Julius <medicjue@gmail.com>
2017-03-18 15:19:33 +0800

    Implement week3 homework

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## This function creates a special "matrix" object that can cache its inverse.
+  inverseOfX = solve(x)
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverseOfX) m <<- inverseOfX
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
+  ## Return a matrix that is the inverse of 'x'
+  inverseOfX <- x$getInverse()
+  if(!is.null(inverseOfX)) {
+    return(inverseOfX)
+  }
+  data <- x$get()
+  inverseOfX <- solve(data)
+  x$setInverse(inverseOfX)
+  return(inverseOfX)
 }
